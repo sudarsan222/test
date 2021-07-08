6d9bafc56e51e80bce67f16938c1af7d3a11854e
sudarsan <javabysudarsan@gmail.com>
2021-07-08 17:53:51 +0530

    hello class

---
+++ src/simple/Hello.java
@@ -0,0 +1,11 @@
+package simple;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("i am in hello class");
+		System.out.println("Hello world");
+	}
+
+}
